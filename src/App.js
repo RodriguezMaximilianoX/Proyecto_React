0100a2a7b69b81d727237f858d3d5575e570713e
Maxi Rodriguez <rodriguezmaximilianoxavier@gmail.com>
2022-05-21 02:46:29 -0300

    coloco nombre a mi tienda

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Gamer Legion</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Todo para convertirte en gamer
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React !!!
         </a>
       </header>
     </div>
