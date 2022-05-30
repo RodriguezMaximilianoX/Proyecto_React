aad5e2435a3a293a368cd1ffc78baec1233eda00
Maxi Rodriguez <rodriguezmaximilianoxavier@gmail.com>
2022-05-30 17:47:31 -0300

    agregando navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar />
         <h1>Gamer Legion</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
