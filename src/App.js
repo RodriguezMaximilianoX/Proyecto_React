64386c9d06d61f4a33d3ae2b5ab1348f3c430bf0
Maxi Rodriguez <rodriguezmaximilianoxavier@gmail.com>
2022-12-01 14:47:28 -0300

    empezando con react

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Volviendo a empezar</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
